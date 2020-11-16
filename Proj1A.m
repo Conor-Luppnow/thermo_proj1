c9c4bd25452298a8d31c284f92d808533f6d993f
Conor Luppnow <conorluppnow@gmail.com>
2020-11-16 15:22:13 -0500

    satisy Gibbs-Duhem eqn

--- Proj1A.m
+++ Proj1A.m
@@ -1,9 +1,10 @@
 function Proj1A
-clc;
+clc; clear; close;
 x1 = 0:0.05:1;
 y1 = [0 0.275 0.436 0.543 0.618 0.675 0.720 0.756 0.786 0.811 0.833 0.853 0.871 0.888 0.903 0.918 0.933 0.949 0.964 0.981 1];
 P_exp = [7295 9562 11695 13682 15536 17256 18883 20390 21817 23150 24404 25604 26751 27845 28898 29925 30938 31939 32952 33979 35032];
-
+T = 313.15;
+R = 8.314;
 
 fun = @(X) P(x1,X,P_exp);
 
@@ -16,6 +17,26 @@ if exitflag > 0
     
 end
 fprintf('A = %5.5f and f = %5.5f \n',A,f);
+
+for j = 1:numel(x1)
+    gamma_1(j) = exp((A/(R*T))*(1-x1(j))^2);
+    gamma_2(j) = exp((A/(R*T))*(x1(j))^2);
+    ln_gamma_ratio(j) = log(gamma_1(j) / gamma_2(j));
+end
+
+
+plot(x1,gamma_1,'or'); hold on
+plot(x1,gamma_2,'ok');  grid on
+
+plot(x1,ln_gamma_ratio,'ob');
+
+xlabel('x1');
+ylabel('\gamma_i');
+legend('\gamma_1','\gamma_2','ln(\gamma_1 / \gamma_2)','Location','North');
+
+area = trapz(x1,ln_gamma_ratio);
+
+fprintf('area = %5.5f',area);
 
 
 end
